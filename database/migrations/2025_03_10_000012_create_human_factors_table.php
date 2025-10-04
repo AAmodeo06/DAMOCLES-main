5a0b935ef3669cca7e6d6d880f3337e3d3ecccd3
AAmodeo06 <136352361+AAmodeo06@users.noreply.github.com>
2025-10-04 17:19:56 +0200

    Migration+Models+modifica

---
+++ database/migrations/2025_03_10_000012_create_human_factors_table.php
@@ -0,0 +1,26 @@
+<?php
+
+//REALIZZATO DA: Andrea Amodeo
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('human_factors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->enum('level', ['none','low', 'medium', 'high', 'max']);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('human_factors');
+    }
+};
